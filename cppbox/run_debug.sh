6d3f327248a2a47621b7e44023571077b0f537c6
Tej Birring <tej.birring@gmail.com>
2021-03-03 03:51:54 +0000

    docker image appended with SSH(D) remote terminal and gdbserver, for debugging inside container from IDE.

--- cppbox/run_debug.sh
+++ cppbox/run_debug.sh
@@ -6,9 +6,17 @@ echo "Running 'cppbox' image for debug..."
 # --cap-add=SYS_PTRACE
 # --security-opt seccomp=unconfined
 
+# Port mapping:
+# map port 8080 (server app)
+# map port 22 (ssh server) on target (container) to 7776 on host (to prevent conflict)
+# map port 7777 (gdb server)
+
 docker run \
 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
 -v "$PROJ_DIR":/usr/src/harvard-references-server \
 -p 8080:8080 \
+-p 7776:22 \
+-p 7777:7777 \
 -e PORT=8080 \
+-e SERVER_DEBUG=1 \
 -ti cppbox:latest bash
